185623e85b401c3794db0244f201e1545cc5778f
Andrei Dodu <6343630+goto-eof@users.noreply.github.com>
2023-12-03 02:43:41 +0100

    Update configuration-sample.rs

--- src/configuration/configuration-sample.rs
+++ src/configuration/configuration-sample.rs
@@ -3,6 +3,6 @@ pub const WIFI_SSID: &str = "";
 // WiFi password
 pub const WIFI_PASS: &str = "";
 // configuration server endpoint -> the endpoint from where ESP32 will download the configuration
-pub const CONFIG_URI: &str = "";
+pub const CONFIG_URI: &str = "http://localhost:8080/api/v1/relay/configuration";
 // checks the configuration every X seconds
 pub const CONFIG_CHECK_INTERVAL_SECONDS: u32 = 3;
